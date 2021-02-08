cebd35a4cece8351a64a8e954030aa87a8a89223
Kevin Sejin <kevin.sejin@teaminternational.com>
2021-02-08 12:09:20 -0500

    Update project

---
+++ SeleniumNetCore/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumNetCore
+{
+    public class Program
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://wwww.google.com");
+        }
+
+
+        [Test]
+        public void Test001_ValidateTittle()
+        {
+            // Arrange
+            string titleExpected = "Google", titleActual = string.Empty;
+
+            // Act
+            titleExpected = titleExpected.ToLower();
+            titleActual = driver.Title.ToLower();
+
+           // Assert
+            Assert.AreEqual(titleExpected, titleActual);
+        }
+
+        [TearDown]
+        public void TestTearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
