530b795c14a4b68f67a9dd3f42841c5b822b218e
Kevin Sejin <kevin.sejin@teaminternational.com>
2021-02-08 12:47:19 -0500

    add automation test

--- SeleniumNetCore/Program.cs
+++ SeleniumNetCore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -9,11 +10,17 @@ namespace SeleniumNetCore
     {
         public IWebDriver driver;
 
-        [SetUp]
-        public void TestSetup()
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Console.WriteLine(@"Running {0}", GetTestName());
             driver.Navigate().GoToUrl("https://google.com");
         }
 
@@ -28,14 +35,103 @@ namespace SeleniumNetCore
             titleExpected = titleExpected.ToLower();
             titleActual = driver.Title.ToLower();
 
-           // Assert
+            // Assert
+            Assert.AreEqual(titleExpected, titleActual);
+        }
+
+        [Test]
+        public void Test002_ValidateSearch()
+        {
+            // Arrange
+            string titleExpected = "selenium - Google Search", titleActual = string.Empty;
+
+            // Act
+            var inputGoogleSearch = driver.FindElement(By.Name("q"));
+            inputGoogleSearch.Clear();
+            inputGoogleSearch.SendKeys("selenium");
+
+            var buttonGoogleSearch = driver.FindElement(By.Name("btnK"));
+            buttonGoogleSearch.Click();
+
+            
+            titleExpected = titleExpected.ToLower();
+            titleActual = driver.Title.ToLower();
+
+            // Assert
             Assert.AreEqual(titleExpected, titleActual);
         }
 
         [TearDown]
-        public void TestTearDown()
+        public void TearDown()
         {
-            driver.Quit();
+            string ResultTest = string.Empty;
+            ResultTest = TestContext.CurrentContext.Result.Outcome.Status.ToString();
+            if (driver == null) return;
+
+            try
+            {
+                //Here the code to capture javascipt errors
+            }
+            finally
+            {
+                if (ResultTest.Equals("Failed"))
+                {
+                    CaptureImage(ResultTest, string.Empty);
+                }
+            }
+            Console.WriteLine(@"" + ResultTest + " {0}", GetTestName());
         }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            if (driver != null)
+            {
+                driver.Manage().Cookies.DeleteAllCookies();
+                driver.Quit();
+            }
+        }
+
+        private static string GetTestName()
+        {
+            return TestContext.CurrentContext.Test.FullName;
+        }
+
+        public string msgerror(string var1, string var2)
+        {
+            return "Error: the values " + var1 + " = " + var2 + " Should be equals";
+        }
+
+        private void CaptureImage(string folder, string captureName)
+        {
+            try
+            {
+                var captureDriver = driver as ITakesScreenshot;
+                if (captureDriver != null)
+                {
+                    var screenshot = captureDriver.GetScreenshot();
+                    var path = Path.Combine(TestContext.CurrentContext.WorkDirectory, folder);
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    string captureSuffix = string.Empty;
+                    if (!string.IsNullOrEmpty(captureName))
+                    {
+                        captureSuffix = "_" + captureName;
+                    }
+
+                    var filename = string.Format("{0}{1}.png", TestContext.CurrentContext.Test.FullName, captureSuffix);
+                    screenshot.SaveAsFile(Path.Combine(path, filename), ScreenshotImageFormat.Png);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+
+
     }
 }
